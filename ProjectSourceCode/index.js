5e00a98dbba04c11b6b5bb6d3698b425d7a84699
Abeneazer Getachew <abge4949@colorado.edu>
2025-12-02 03:16:25 -0700

    Use POSTGRES_* env vars instead of db_adinm

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -27,11 +27,11 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-  host: process.env.POSTGRES_HOST || "db", // the database server
-  port: 5432, // the database port
-  database: process.env.POSTGRES_DB, // the database name
-  user: process.env.POSTGRES_USER, // the user account to connect with
-  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+  host: process.env.POSTGRES_HOST || "db",
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
 };
 
 const db = pgp(dbConfig);
--- ProjectSourceCode/init_data/create.sql
+++ ProjectSourceCode/init_data/create.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,  -- Changed from user_name to username
+    username VARCHAR(50) NOT NULL UNIQUE, 
     password VARCHAR(255) NOT NULL
 );
 
